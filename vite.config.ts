84bf7de47ea16a64aab65c25d5a02608ba57ebd0
Johannes Bremer <jb@hausarzt-mvz-dissen.de>
2025-06-15 11:59:58 +0200

    Revert adding `autoCodeSplitting` bc it causes regression in `/praxisplaner`

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,4 @@
 import { tanstackStart } from "@tanstack/react-start-plugin";
-import { tanstackRouter } from "@tanstack/router-plugin/vite";
-import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -15,10 +13,6 @@ export default defineConfig({
         srcDirectory: "src",
       },
     }),
-    tanstackRouter({
-      autoCodeSplitting: true,
-    }),
-    react(),
     visualizer({
       filename: "bundle-analysis.json",
       template: "raw-data",
