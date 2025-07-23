0c056f6f9af148d26a0a3c7e16b5e65965cff3c3
Johannes Bremer <jb@hausarzt-mvz-dissen.de>
2025-07-23 11:09:07 +0200

    Credit upstream

--- src/components/version-graph/types.ts
+++ src/components/version-graph/types.ts
@@ -1,3 +1,8 @@
+/*
+This file is forked from the following MIT licensed repo:
+https://github.com/liuliu-dev/CommitGraph/tree/0f89c35fa53003ed8b66b409230566a455d85202
+*/
+
 import type { Id } from "@/convex/_generated/dataModel";
 
 /** Represents one saved state in the version history. */
