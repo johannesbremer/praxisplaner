67766f8f474db5f5244056b04a1f83bbfdadc51b
Johannes Bremer <jb@hausarzt-mvz-dissen.de>
2025-09-28 15:10:56 +0200

    German theme toggle labels

--- components/mode-toggle.tsx
+++ components/mode-toggle.tsx
@@ -18,7 +18,7 @@ export function ModeToggle() {
         <Button size="icon" variant="outline">
           <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Toggle theme</span>
+          <span className="sr-only">Design wechseln</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
@@ -27,14 +27,14 @@ export function ModeToggle() {
             setTheme("light");
           }}
         >
-          Light
+          Hell
         </DropdownMenuItem>
         <DropdownMenuItem
           onClick={() => {
             setTheme("dark");
           }}
         >
-          Dark
+          Dunkel
         </DropdownMenuItem>
         <DropdownMenuItem
           onClick={() => {
